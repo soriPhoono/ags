b71af0c0c55a6fcf1006eaa53a59b570bc4fee40
soriphoono <soriphoono@gmail.com>
2024-10-21 15:41:25 -0500

    First commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ags = {
+      url = "github:Aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixd
+            nixpkgs-fmt
+
+
+          ];
+        };
+      };
+      flake = {
+        homeManagerModules.default = {
+          imports = [
+            ./ags.nix
+          ];
+        };
+      };
+    };
+}
